87e8241ee8677e02addaba509667092a313e174a
Ohleh <oleg.kolischuk@gmail.com>
2024-02-01 21:12:49 +0200

    comlete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galleryRef = document.querySelector("ul.gallery")
+
+function renderCreate (galleryItems) {
+    return galleryItems.reduce((acc, {preview, original, description}) => acc + `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a> `,"")
+}
+
+galleryRef.insertAdjacentHTML("afterbegin", renderCreate(galleryItems));
+
+function handleGallery(event) {
+    event.preventDefault();
+   if (!event.target.dataset.source) return;
+   const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+    )
+   instance.show()
+   window.addEventListener("keydown", onCloseImage)
+
+
+   function onCloseImage (event) {
+
+    if (event.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onCloseImage);
+    }
+     
+  }
+}
+
+galleryRef.addEventListener("click", handleGallery)
+
+
+
+
+
+// function onCloseImage (event) {
+//   console.log(event.code)
+//   if (event.code.key === "Escape")
+//   instance.close()
+// }
+// window.addEventListener("keydown", onCloseImage)
+
+
+
+
+
+// instance.show()
+// instance.close()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector("ul.gallery")
 
-console.log(galleryItems);
+
+
+
+function renderCreate (galleryItems) {
+    return galleryItems.reduce((acc, {preview, original, description}) => acc + `
+    <li class="gallery__item" class="gallery">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+
+ </li>`,"")
+}
+
+galleryRef.insertAdjacentHTML("afterbegin", renderCreate(galleryItems));
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+function handleGallery(event) {
+    event.preventDefault();
+gallery.on('show.simplelightbox', function (e) {});
+}
+
+galleryRef.addEventListener("click", handleGallery)
